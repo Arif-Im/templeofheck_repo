3bfb72df96c07ba7840a6ed27e3cca4db26dda0f
Arif-Im <60347931+Arif-Im@users.noreply.github.com>
2021-01-29 16:33:23 +0800

    TempleOfHeck_Project
    This is the main project.

---
+++ TempleOfHeck_Ghostbusters/Assets/GridMovement2D.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement2D : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] Transform movePoint;
+    [SerializeField] LayerMask whatIsStopMovement;
+    [SerializeField] float drawCircleRadius = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f) / 2, drawCircleRadius, whatIsStopMovement))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+                }
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f) / 2, drawCircleRadius, whatIsStopMovement))
+                {
+                    movePoint.position += new Vector3(0, Input.GetAxisRaw("Vertical"), 0);
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f) / 2 + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f) / 2, drawCircleRadius);
+    }
+}
