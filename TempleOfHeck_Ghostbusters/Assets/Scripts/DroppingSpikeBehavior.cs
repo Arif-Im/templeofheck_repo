926c7f42607a9b0a65ee881cb758d15a53f4faa0
WangHaiFeng <hfeng1998@gmail.com>
2021-01-31 17:14:02 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/Arif-Im/templeofheck_repo""
    This reverts commit fd84326dbc2d2a20b21b33172b5a336c86a09530.

--- TempleOfHeck_Ghostbusters/Assets/Scripts/DroppingSpikeBehavior.cs
+++ TempleOfHeck_Ghostbusters/Assets/Scripts/DroppingSpikeBehavior.cs
@@ -5,41 +5,91 @@ using UnityEngine;
 public class DroppingSpikeBehavior : MonoBehaviour
 {
     [SerializeField] float startingKillTime;
+    Animator anim;
     float killTime;
+    bool isFalling = false;
+    GameObject shadow;
+
+    float shadowScaleX;
+    float shadowScaleY;
+
+    [SerializeField] Vector3 targetSize;
+    Vector3 startingSize;
+
+    public Death player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
+        shadow = transform.GetChild(1).gameObject;
+        startingSize = shadow.transform.localScale;
+        shadowScaleX = shadow.transform.localScale.x;
+        shadowScaleY = shadow.transform.localScale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (isFalling)
+        {
+            if (killTime > 0)
+            {
+                killTime -= Time.deltaTime;
+                if(shadowScaleX < targetSize.x && shadowScaleY < targetSize.y)
+                {
+                    shadowScaleX += Time.deltaTime;
+                    shadowScaleY += Time.deltaTime;
+                    shadow.transform.localScale = new Vector3(shadowScaleX, shadowScaleY, shadow.transform.localScale.z);
+                }
+            }
+            else
+            {
+                anim.SetTrigger("dropSpike");
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<GridMovement2D>())
+        if (collision.GetComponent<Death>())
         {
+            isFalling = true;
             killTime = startingKillTime;
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.GetComponent<GridMovement2D>())
+        if (collision.GetComponent<Death>())
         {
-            if (killTime > 0)
-            {
-                killTime -= Time.deltaTime;
-            }
-            else
-            {
+            player = collision.GetComponent<Death>();
 
-                collision.gameObject.GetComponent<Death>().Activate();
-            }
+            //if (killTime > 0)
+            //{
+            //    killTime -= Time.deltaTime;
+            //}
+            //else
+            //{
+            //    anim.SetTrigger("dropSpike");
+            //}
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Death>())
+        {
+            player = null;
+
+            //if (killTime > 0)
+            //{
+            //    killTime -= Time.deltaTime;
+            //}
+            //else
+            //{
+            //    anim.SetTrigger("dropSpike");
+            //}
         }
     }
 }
