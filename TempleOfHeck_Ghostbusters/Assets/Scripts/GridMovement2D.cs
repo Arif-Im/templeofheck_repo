980c37f36b2dee3c91b83cbd71f492a521139391
Guluere <32575902+Guluere@users.noreply.github.com>
2021-01-31 14:03:31 +0800

    Revert "Update GridMovement2D.cs"
    This reverts commit f63bf6839b08aac0b5c1b7674dcdbea77a25cb4a.

--- TempleOfHeck_Ghostbusters/Assets/Scripts/GridMovement2D.cs
+++ TempleOfHeck_Ghostbusters/Assets/Scripts/GridMovement2D.cs
@@ -57,6 +57,11 @@ public class GridMovement2D : MonoBehaviour
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05)
         {
+            if(levelComplete)
+            {
+                playerAnim.SetBool(runningID, false);
+                this.enabled = false;
+            }
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
             {
                 if (!blockTouchWall)
@@ -93,12 +98,6 @@ public class GridMovement2D : MonoBehaviour
             else
             {
                 playerAnim.SetBool(runningID, false);
-            }
-
-            if (levelComplete)
-            {
-                playerAnim.SetBool(runningID, false);
-                this.enabled = false;
             }
         }
     }
