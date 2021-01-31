f09af54c1e4a1a768a2463451e477d48b446417b
WangHaiFeng <hfeng1998@gmail.com>
2021-01-31 17:51:45 +0800

    Game Manager Starts Timer if not first Level
    Level 3 Added Event System for Back Button.

--- TempleOfHeck_Ghostbusters/Assets/Scripts/GameManager.cs
+++ TempleOfHeck_Ghostbusters/Assets/Scripts/GameManager.cs
@@ -25,6 +25,10 @@ public class GameManager : MonoBehaviour
             if (firstScene != null)
                 firstScene.Invoke();
         }
+        else
+        {
+            StartTimer();
+        }
         if (timer != null)
             timer.text = deathTime.ToString();
     }
