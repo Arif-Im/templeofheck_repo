6350e33a73c2ebc6f020d6f1397c317fad5f0f85
WangHaiFeng <hfeng1998@gmail.com>
2021-02-07 22:58:00 +0800

    Persistent Theme Music added in Level 1
    The ThemeMusic is Loaded in Level1 and is persistent across scenes using the PersistentMusic script. It will destroy duplicate loads of ThemeMusic GameObjects.

---
+++ TempleOfHeck_Ghostbusters/Assets/Scripts/PersistentMusic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentMusic : MonoBehaviour
+{
+    public static PersistentMusic Instance; // Instance of Theme Music
+
+    void Awake()
+    {
+        // Destroy if an Instance exists or assign this as Instance if not
+        DontDestroyOnLoad(this);
+        if (PersistentMusic.Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
