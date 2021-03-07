8aeb9c689e0e4b7258e31a7a8e4bc8128dd0fc4a
WangHaiFeng <hfeng1998@gmail.com>
2021-03-08 01:35:37 +0800

    Updated KillPlayer.cs with Safe and Kill States
    Spikes now have Safe states where it is safe to walk on them and Kill states where it is dangerous to walk on them. The duration for either state can be set in the editor.

--- TempleOfHeck_Ghostbusters/Assets/Scripts/KillPlayer.cs
+++ TempleOfHeck_Ghostbusters/Assets/Scripts/KillPlayer.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class KillPlayer : MonoBehaviour
 {
+    // Variables for delays between kill states
+    [SerializeField]
+    private float safeDuration = 0.0f; // Delay between kill states
+    [SerializeField]
+    private float killDuration = 0.0f; // Duration of kill state
+    private float lastStateTransitionTime = 0.0f; // Time of state transition
+    private bool killState = false; // State of kill
+    public Animator anim = null;
+    private int safeID;
+
     float raycastDistance = 1.5f;
     LayerMask pushableBlock;
     GridMovement2D player;
@@ -12,19 +22,41 @@ public class KillPlayer : MonoBehaviour
     void Start()
     {
         player = FindObjectOfType<GridMovement2D>();
+        lastStateTransitionTime = Time.time;
+        safeID = Animator.StringToHash("Safe");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        // Transition between safe and kill states with different durations for both
+        if(safeDuration > 0 && killState)
+        {
+            if(Time.time - lastStateTransitionTime >= killDuration)
+            {
+                killState = !killState;
+                lastStateTransitionTime = Time.time;
+                if (anim != null)
+                    anim.SetBool(safeID, true);
+            }
+        }
+        else
+        {
+            if (Time.time - lastStateTransitionTime >= safeDuration)
+            {
+                killState = !killState;
+                lastStateTransitionTime = Time.time;
+                if (anim != null)
+                    anim.SetBool(safeID, false);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.GetComponent<Death>())
         {
-            player.gameObject.GetComponent<Death>().Activate();
+            if(killState)
+                player.gameObject.GetComponent<Death>().Activate();
             /*
             player.gameObject.SetActive(false);
             StartCoroutine("Respawn");
